41392c5a606eff7e344a9bf224ae8aa9f0706957
Younes <66238240+younescs@users.noreply.github.com>
2025-07-07 20:57:01 -0400

    Time: 982 ms (5.02%) | Memory: 23.4 MB (12.07%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        list1 = copy.deepcopy(nums)
+        list2 = copy.deepcopy(nums)
+        output = [0]*len(nums)
+
+   
+
+        for i in range(1, len(nums), 1):
+            list1[i] = list1[i-1]* list1[i]
+        
+        print(list1)
+
+        for i in range(len(nums) - 2, -1, -1):
+            list2[i] = list2[i]* list2[i+1]
+
+        print(list2)
+
+        output[len(nums)- 1] = list1[len(nums)- 2]
+        output[0] = list2[1]
+        for i in range(1, len(output) - 1, 1):
+            output[i] = list1[i-1] * list2[i+1]
+
+        return output
