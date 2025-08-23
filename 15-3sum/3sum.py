58b0cd020285f7761a13aba6ceeeda6a28030c90
Younes <66238240+younescs@users.noreply.github.com>
2025-08-23 02:36:34 -0400

    Time: 1627 ms (20.37%) | Memory: 18.9 MB (24.40%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,18 +1,37 @@
 class Solution(object):
     def threeSum(self, nums):
-        finalAnswers = set()
-        
-        for a in range(len(nums)):
-            anchor = nums[a]
-            dico2sum = {}
+    
+        nums.sort()
+        res = []
+        anchor = 0
+        while anchor < len(nums) - 2:
+
+            if anchor > 0 and nums[anchor] == nums[anchor-1]:
+                anchor += 1
+                continue
+
+            l = anchor +1
+            r = len(nums)-1
+
+            while l < r:
+                if nums[anchor] + nums[l] + nums[r] == 0:
+                    res.append([nums[anchor],nums[l],nums[r]])
+                    l += 1
+                    r -=1
+                    while l<r and nums[l] == nums[l-1]:
+                        l+=1
+                    while l<r and nums[r] == nums[r+1]:
+                        r-=1
+
+                elif nums[anchor] + nums[l] + nums[r] < 0:
+                    l += 1
+                    
+
+                elif nums[anchor] + nums[l] + nums[r] > 0:
+                    r -= 1
+
+            anchor +=1
+
+        return res
+
             
-            for i in range(a+1, len(nums)):
-                target = - (anchor + nums[i])
-                
-                if target in dico2sum:
-                    triplet = tuple(sorted([anchor, nums[i], target]))
-                    finalAnswers.add(triplet)
-                else:
-                    dico2sum[nums[i]] = i
-        
-        return list(finalAnswers)
