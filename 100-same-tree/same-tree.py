6a4aace9b0e337d7c1c38026ff5fd878d4d49fba
Younes <66238240+younescs@users.noreply.github.com>
2025-09-14 00:48:49 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (86.48%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -12,18 +12,19 @@ class Solution(object):
         :rtype: bool
         """
 
-        
-
-        def check(root, root2):
-            if root == None and root2 == None:
+        def checkTree(tree1, tree2):
+            
+            if not tree1 and not tree2:
                 return True
-            if root == None or root2 == None:
+            if not tree1 or not tree2:
                 return False
-            if root.val != root2.val:
+
+            if tree1.val != tree2.val:
                 return False
-            return check(root.left, root2.left) and check(root.right, root2.right)
-                
 
+            return checkTree(tree1.left, tree2.left) and checkTree(tree1.right, tree2.right)
 
-        return check(p, q)
-                
+        return checkTree(p, q)
+
+        
+        
