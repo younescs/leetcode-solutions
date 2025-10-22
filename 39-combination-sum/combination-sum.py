b2d5f18723591b5214974feb7f78835b982dc552
Younes <66238240+younescs@users.noreply.github.com>
2025-10-21 20:47:42 -0700

    Time: 24 ms (33.05%) | Memory: 12.5 MB (80.48%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        res = []
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur[:])
+                return
+            if i >= len(candidates) or total > target:
+                return
+            
+            candidates[i]
+
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+        return res
