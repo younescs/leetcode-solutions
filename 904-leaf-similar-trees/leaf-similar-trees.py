3ce5cde53a5d78c8a656fbac3aa855b52e4f3006
Younes <66238240+younescs@users.noreply.github.com>
2025-11-11 01:35:26 -0500

    Time: 3 ms (20.59%) | Memory: 12.6 MB (70.89%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -14,22 +14,20 @@ class Solution(object):
 
         self.leaves = []
         self.leaves2 = []
-
+        
         def dfs(root, alist):
+
             if not root:
                 return
             if not root.left and not root.right:
                 alist.append(root.val)
-            else:
-                dfs(root.left, alist)
-                dfs(root.right, alist)
-        
+                return
+            dfs(root.left, alist)
+            dfs(root.right, alist)
 
+ 
         dfs(root1, self.leaves)
         dfs(root2, self.leaves2)
 
-
         return self.leaves == self.leaves2
-        
-
-
+            
